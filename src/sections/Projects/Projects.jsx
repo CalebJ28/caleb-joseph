dc2c3b84f950383f9809fcf6921e5c2a5919be55
Caleb Joseph <124648231+CalebJ28@users.noreply.github.com>
2024-07-31 00:43:42 -0400

    Updates to projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,16 @@
 import styles from './ProjectsStyles.module.css'
-import viberr from '../../assets/viberr.png'
-import freshBurger from '../../assets/fresh-burger.png'
+import robo from '../../assets/robo.png'
+import heli from '../../assets/cccce.png'
+import jav from '../../assets/dawdad.png'
 import ProjectCard from '../../common/ProjectCard'
 
 function Projects() {
   return ( <section id="projects" className={styles.container}>
     <h1 className="sectionTitle">Projects</h1>
     <div className={styles.projectsContainer}>
-        < ProjectCard src={viberr} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Filler" p="Test project"/>
-        < ProjectCard src={freshBurger} link="https://github.com/CalebJ28/ELE404" h3="Filler" p="Test project"/>
-        < ProjectCard src={viberr} link="https://github.com/CalebJ28/COE318" h3="Filler" p="Test project"/>
+        < ProjectCard src={robo} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Autonomous Bluetooth Vehicle" p="Arduino C++/C Robot Car project"/>
+        < ProjectCard src={heli} link="https://github.com/CalebJ28/HelicopterEscapes" h3="HelicopterEscapes" p="Python Data Visualization project"/>
+        < ProjectCard src={jav} link="https://github.com/CalebJ28/COE528/tree/main/coe528Lab2" h3="Procedureal Abstraction" p="Java course project"/>
     </div>
     </section>
   )
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,28 +5,41 @@ import SkillList from '../../common/SkillList'
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
-        <h1 className='sectionTitle'>Skills</h1>
+        <h1 className='sectionTitle'>Technical Skills</h1>
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="Python" />
+            <SkillList src={checkMarkIcon} skill="Java" />
+            <SkillList src={checkMarkIcon} skill="C/C++" />
+            <SkillList src={checkMarkIcon} skill="VHDL" />
+            <SkillList src={checkMarkIcon} skill="Verilog" />
+            <SkillList src={checkMarkIcon} skill="Perl" />
+            <SkillList src={checkMarkIcon} skill="Bash/CSH" />
+        </div>
+        <hr />
         <div className={styles.skillList}>
             <SkillList src={checkMarkIcon} skill="HTML" />
             <SkillList src={checkMarkIcon} skill="CSS" />
             <SkillList src={checkMarkIcon} skill="JavaScript" />
-            <SkillList src={checkMarkIcon} skill="TypeScript" />
             <SkillList src={checkMarkIcon} skill="Node" />
-        </div>
-        <hr />
-        <div className={styles.skillList}>
             <SkillList src={checkMarkIcon} skill="React" />
-            <SkillList src={checkMarkIcon} skill="Angular" />
-            <SkillList src={checkMarkIcon} skill="Vue" />
             <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
         </div>
         <hr />
         <div className={styles.skillList}>
-            <SkillList src={checkMarkIcon} skill="Redux" />
-            <SkillList src={checkMarkIcon} skill="Webpack" />
-            <SkillList src={checkMarkIcon} skill="Git" />
-            <SkillList src={checkMarkIcon} skill="Jest" />
-            <SkillList src={checkMarkIcon} skill="Bootstrap" />
+            <SkillList src={checkMarkIcon} skill="Arduino" />
+            <SkillList src={checkMarkIcon} skill="RTL" />
+            <SkillList src={checkMarkIcon} skill="ASIC/FPGA" />
+            <SkillList src={checkMarkIcon} skill="UVM" />
+            <SkillList src={checkMarkIcon} skill="Linux" />
+            <SkillList src={checkMarkIcon} skill="UNIX" />
+        </div>
+        <hr />
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="Verification" />
+            <SkillList src={checkMarkIcon} skill="Validation" />
+            <SkillList src={checkMarkIcon} skill="Debugging" />
+            <SkillList src={checkMarkIcon} skill="Scripting" />
+            <SkillList src={checkMarkIcon} skill="Computer Architecture" />
         </div>
     </section>
 
