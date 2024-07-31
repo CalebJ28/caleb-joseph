9cdad1e866068bacf25ce7d868e52c0ad9f3a0ad
Caleb Joseph <124648231+CalebJ28@users.noreply.github.com>
2024-07-30 22:48:45 -0400

    test

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import styles from './ProjectsStyles.module.css'
-import viberr from '../../assets/viberr.png'
+import robo from '../../assets/robo.png'
 import freshBurger from '../../assets/fresh-burger.png'
 import ProjectCard from '../../common/ProjectCard'
 
@@ -7,7 +7,7 @@ function Projects() {
   return ( <section id="projects" className={styles.container}>
     <h1 className="sectionTitle">Projects</h1>
     <div className={styles.projectsContainer}>
-        < ProjectCard src={viberr} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Filler" p="Test project"/>
+        < ProjectCard src={robo} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Autonomous-Bluetooth-Vehicle-" p="Arduino C++/C Bluetooth Car"/>
         < ProjectCard src={freshBurger} link="https://github.com/CalebJ28/ELE404" h3="Filler" p="Test project"/>
         < ProjectCard src={viberr} link="https://github.com/CalebJ28/COE318" h3="Filler" p="Test project"/>
     </div>
