f3e6be7079542109cf8c1f01c7d012d92ae4dd72
Caleb Joseph <124648231+CalebJ28@users.noreply.github.com>
2024-08-18 00:44:40 -0400

    updated project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,16 @@
 import styles from './ProjectsStyles.module.css'
 import robo from '../../assets/robo.png'
 import heli from '../../assets/cccce.png'
-import jav from '../../assets/dawdad.png'
+import jav from '../../assets/raytt.png'
 import ProjectCard from '../../common/ProjectCard'
 
 function Projects() {
   return ( <section id="projects" className={styles.container}>
     <h1 className="sectionTitle">Projects</h1>
     <div className={styles.projectsContainer}>
-        < ProjectCard src={robo} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Autonomous Bluetooth Vehicle" p="Arduino C++/C Robot Car project"/>
-        < ProjectCard src={heli} link="https://github.com/CalebJ28/HelicopterEscapes" h3="HelicopterEscapes" p="Python Data Visualization project"/>
-        < ProjectCard src={jav} link="https://github.com/CalebJ28/COE528/tree/main/coe528Lab2" h3="Procedureal Abstraction" p="Java course project"/>
+        < ProjectCard src={robo} link="https://github.com/CalebJ28/Autonomous-Bluetooth-Veichle-" h3="Autonomous Bluetooth Vehicle" p="Arduino, C++/C, Robot Car project"/>
+        < ProjectCard src={heli} link="https://github.com/CalebJ28/HelicopterEscapes" h3="HelicopterEscapes" p="Python, Data Visualization project"/>
+        < ProjectCard src={jav} link="https://github.com/CalebJ28/RayTracing/tree/master" h3="RayTracing Application" p="RayTracing, GPU & C/C++ project"/>
     </div>
     </section>
   )
